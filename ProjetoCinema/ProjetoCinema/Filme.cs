90302a2da134bfd1cb4cc2e3d7332e025aa7497e
Victor <victor.c@aluno.ifsp.edu.br>
2018-10-17 14:39:37 -0300

    Inicio do projeto. -Classes criadas:     -Filme: Apenas criada, sem implementação nenhuma.    -Sala: Apenas criada, sem implementação nenhuma.    -Sessao: Apenas criada, sem implementação nenhuma.

---
+++ ProjetoCinema/ProjetoCinema/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCinema
+{
+    class Filme
+    {
+        private string nome;
+        private string genero;
+        private string dataLancamento;
+    }
+}
---
+++ ProjetoCinema/ProjetoCinema/Sessao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCinema
+{
+    class Sessao
+    {
+        private int numeroSessao;
+
+        public Sessao(Filme f,Sala s)
+        {
+
+        }
+    }
+}
