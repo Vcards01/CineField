c22e45e222b732f41cce7f57da81d754e3df11b3
rosundfeld <rodrigo.psundfeld@gmail.com>
2018-10-30 01:22:21 -0300

    raRAW

--- ProjetoCinema/ProjetoCinema/FormPrintIngresso.Designer.cs
+++ ProjetoCinema/ProjetoCinema/FormPrintIngresso.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.gpFilmes = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnFilmes = new System.Windows.Forms.Panel();
+            this.lbl_ingresso = new System.Windows.Forms.Label();
             this.pnFilmesBaixo = new System.Windows.Forms.Panel();
             this.i = new System.Windows.Forms.Button();
-            this.lbl_ingresso = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.gpFilmes.SuspendLayout();
             this.pnFilmes.SuspendLayout();
             this.pnFilmesBaixo.SuspendLayout();
@@ -52,6 +52,24 @@
             this.gpFilmes.TabStop = false;
             this.gpFilmes.Text = "Ingresso";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(418, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Aqui você pega seu ingresso no futuro";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(226, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
             // pnFilmes
             // 
             this.pnFilmes.BackgroundImage = global::ProjetoCinema.Properties.Resources.fundo1;
@@ -60,6 +78,18 @@
             this.pnFilmes.Name = "pnFilmes";
             this.pnFilmes.Size = new System.Drawing.Size(511, 61);
             this.pnFilmes.TabIndex = 6;
+            // 
+            // lbl_ingresso
+            // 
+            this.lbl_ingresso.AutoSize = true;
+            this.lbl_ingresso.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_ingresso.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ingresso.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_ingresso.Location = new System.Drawing.Point(131, 17);
+            this.lbl_ingresso.Name = "lbl_ingresso";
+            this.lbl_ingresso.Size = new System.Drawing.Size(240, 29);
+            this.lbl_ingresso.TabIndex = 0;
+            this.lbl_ingresso.Text = "Pegue seu ingresso";
             // 
             // pnFilmesBaixo
             // 
@@ -79,36 +109,6 @@
             this.i.Text = "Imprimir ingresso!";
             this.i.UseVisualStyleBackColor = true;
             // 
-            // lbl_ingresso
-            // 
-            this.lbl_ingresso.AutoSize = true;
-            this.lbl_ingresso.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_ingresso.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_ingresso.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lbl_ingresso.Location = new System.Drawing.Point(131, 17);
-            this.lbl_ingresso.Name = "lbl_ingresso";
-            this.lbl_ingresso.Size = new System.Drawing.Size(240, 29);
-            this.lbl_ingresso.TabIndex = 0;
-            this.lbl_ingresso.Text = "Pegue seu ingresso";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(226, 147);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
-            this.label1.TabIndex = 0;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(36, 135);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(418, 25);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Aqui você pega seu ingresso no futuro";
-            // 
             // FormPrintIngresso
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -117,6 +117,7 @@
             this.Controls.Add(this.pnFilmes);
             this.Controls.Add(this.pnFilmesBaixo);
             this.Controls.Add(this.gpFilmes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "FormPrintIngresso";
             this.Text = "FormPrintIngresso";
             this.gpFilmes.ResumeLayout(false);
