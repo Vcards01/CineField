becf57ccbb3e6641421d9a75aa7cc3f1cf3e4466
IFSP <IFSP@IFSP-107091>
2018-11-05 21:05:01 -0200

    zz

--- ProjetoCinema/ProjetoCinema/Form1.Designer.cs
+++ ProjetoCinema/ProjetoCinema/Form1.Designer.cs
@@ -201,7 +201,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(896, 441);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.Name = "Form1";
